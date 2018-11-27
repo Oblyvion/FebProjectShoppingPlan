180a7e3254e14261d8dbacb19a925b04f66e8a90
Julian Fess <fessjulian204@gmail.com>
2018-11-27 12:05:00 +0100

    build base

--- app/src/main/java/de/feb/projectshoppingplan/shopItem.java
+++ app/src/main/java/de/feb/projectshoppingplan/shopItem.java
@@ -3,5 +3,8 @@ package de.feb.projectshoppingplan;
 public class shopItem {
 
     String name;
+    String description;
+
+    //https://medium.com/@ruut_j/a-recyclerview-with-multiple-item-types-bce7fbd1d30e
 
 }
