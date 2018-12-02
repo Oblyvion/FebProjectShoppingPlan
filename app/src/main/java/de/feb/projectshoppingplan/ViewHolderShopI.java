740d7719ccb6ba2c8dfa163b49d5149d0508494e
Oblyvion <weber_f-j@live.de>
2018-12-02 03:57:01 +0100

    some things changed

--- app/src/main/java/de/feb/projectshoppingplan/ViewHolderShopI.java
+++ app/src/main/java/de/feb/projectshoppingplan/ViewHolderShopI.java
@@ -1,8 +1,11 @@
 package de.feb.projectshoppingplan;
 
 import android.database.DataSetObserver;
+import android.graphics.PixelFormat;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.PixelCopy;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
